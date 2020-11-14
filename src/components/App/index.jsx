ccd2b2ee1b899e2afa1e3ebc27a828e006c8d78b
Philip Fol <philip.fol08@gmail.com>
2020-11-14 09:39:33 +0000

    Format code with Prettier

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -1,20 +1,20 @@
-import React, { useState, useEffect } from 'react'
-import Card from '../Card'
-import fetchData from  '../../utils/fetchData'
+import React, { useState, useEffect } from "react";
+import Card from "../Card";
+import fetchData from "../../utils/fetchData";
 
 const App = () => {
-  const [cards, setCards] = useState([])
+  const [cards, setCards] = useState([]);
 
-  useEffect(() => fetchData(setCards), [])
+  useEffect(() => fetchData(setCards), []);
 
   return (
     <div className="app">
       <h1>This is an interactive grid</h1>
-      {cards.map(card => (
-        <Card cardTitle={card.title}/>
+      {cards.map((card) => (
+        <Card cardTitle={card.title} />
       ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from "react";
+import "./Card.css";
 
-const Card = props => (
+const Card = (props) => (
   <div className="card">
     <div className="card-title"> {props.cardTitle} </div>
     <div className="card-thumbnail"> Thumbnail goes here </div>
   </div>
-)
+);
 
-export default Card
+export default Card;
